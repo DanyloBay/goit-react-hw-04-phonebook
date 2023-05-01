baa5dc8d140412bff7bc30863a76bbfc336a0f10
Danylo Bay <danylobay@gmail.com>
2023-05-01 07:17:10 +0200

    updated form

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -39,6 +39,7 @@ export const ContactForm = ({ onSubmit }) => {
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
+            value={name}
             onChange={handleInputChange}
           />
         </label>
@@ -50,6 +51,7 @@ export const ContactForm = ({ onSubmit }) => {
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
+            value={number}
             onChange={handleInputChange}
           />
         </label>
